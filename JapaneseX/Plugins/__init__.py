da6de3e47f56a94aef2127b686fdb3da570c8797
NOBITA_XD <156512147+nobitaaxd@users.noreply.github.com>
2024-02-03 13:25:44 +0530

    Create __init__.py

---
+++ JapaneseX/Plugins/__init__.py
@@ -0,0 +1,10 @@
+from JapaneseX.helpers import functions as simpdef
+from NOBITA_XD import *
+from NOBITA_XD.JapaneseX import *
+from JapaneseX import *
+from JapaneseX.utils import *
+from JapaneseX import *
+id = 6694740726
+userid = 6694740726
+simpmusic = simpdef.simpmusic 
+simpmusicvideo = simpdef.simpmusicvideo
