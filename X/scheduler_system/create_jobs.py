3dc5a088abedfa00abe3d1eee6bf89602127aea0
NOBITA_XD <156512147+nobitaaxd@users.noreply.github.com>
2024-02-08 13:40:54 +0530

    Update create_jobs.py

--- X/scheduler_system/create_jobs.py
+++ X/scheduler_system/create_jobs.py
@@ -1,1 +1,10 @@
+import X
+from X import scheduler
 
+
+def set_client(new_client):
+    X.client = new_client
+
+
+def add_job(function, seconds=3):
+    scheduler.add_job(function, "interval", seconds=seconds, args=[X.client])
