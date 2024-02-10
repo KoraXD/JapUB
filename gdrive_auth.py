1b012313c33b5030032dedaa0e53c30db7c13518
NOBITA_XD <156512147+nobitaaxd@users.noreply.github.com>
2024-02-10 15:39:34 +0530

    Delete gdrive_auth.py

--- gdrive_auth.py
+++
@@ -1,7 +0,0 @@
-from pydrive.auth import GoogleAuth
-
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
-gauth.SaveCredentialsFile('creds.txt')
-
-print(f'creds.text File has been created. Check the file')
